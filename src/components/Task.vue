6efd795637690b997b348a4ebb41d295c654a635
Ildar <47642679+hushdev@users.noreply.github.com>
2021-03-08 22:26:42 +0500

    Fixed css bugs

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,18 +1,18 @@
 <template>
-        <li class="task" :class="{ lineThrought: task.isActive == false }">
-            {{ number + 1 }}. {{ task.text }}
-            <span class="actions">
-                <button @click="editTask">
-                    <img src="@/assets/pencil.svg" alt="" />
-                </button>
-                <button @click="markTask">
-                    <img src="@/assets/complete.svg" alt="" />
-                </button>
-                <button @click="deleteTask">
-                    <img src="@/assets/trash.svg" alt="" />
-                </button>
-            </span>
-        </li>
+    <li class="task" :class="{ lineThrought: task.isActive == false }">
+        {{ number + 1 }}. {{ task.text }}
+        <span class="actions">
+            <button @click="editTask">
+                <img src="@/assets/pencil.svg" alt="" />
+            </button>
+            <button @click="markTask">
+                <img src="@/assets/complete.svg" alt="" />
+            </button>
+            <button @click="deleteTask">
+                <img src="@/assets/trash.svg" alt="" />
+            </button>
+        </span>
+    </li>
 </template>
 
 <script>
@@ -82,6 +82,9 @@ export default {
     text-decoration: line-through;
     color: #6c6c6c;
 }
-
-
+@media (max-width: 1160px) {
+    .task {
+        margin-right: 20px;
+    }
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
     // publicPath: '/vue-task-manager/',
-    // baseUrl: '/'
 }
